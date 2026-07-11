f1f4c3b76543a1f380dd1f96cfc792288fd14992
Jamie Lindner <jamievlin@outlook.com>
2026-07-11 00:52:18 -0600

    HELPER: Add examples with threads.

---
+++ examples/threadSample.cc
@@ -0,0 +1,73 @@
+#include <asyffi.h>
+#include "asyffihelper.h"
+
+#include <random>
+#include <vector>
+#include <thread>
+
+// on windows, compile as a DLL file
+// on linux, compile with
+// g++ -shared -std=c++20 -fPIC -o libasydll.so asydll.cc
+
+DECLARE_REGISTER_FN(threadSample);
+
+namespace
+{
+
+ASY_FOREIGN_FUNC_SIG(createRandomPoints)
+{
+    ASYFFI_CONTEXT_HELPER
+
+    auto numberOfPoints = AsyFfiHelpers::Item::getItem<int64_t>(args->getNumberedArg(0));
+    IAsyArray* newArray = context->createNewArray(numberOfPoints);
+
+    size_t const threadCount = std::thread::hardware_concurrency();
+    std::vector<std::thread> threads;
+    threads.reserve(threadCount);
+
+    std::random_device rd;
+    for (size_t i = 0; i < threadCount; ++i)
+    {
+        threads.emplace_back(ctxHelper.createNewThread(
+            [context, newArray, threadCount,
+             numberOfPoints](unsigned int const seed, size_t const idx)
+            {
+                std::mt19937_64 randEng(seed);
+                std::uniform_real_distribution<> randDist(-100.0, 100.0);
+
+                for (size_t runIndex = idx; runIndex < numberOfPoints; runIndex += threadCount)
+                {
+                    IAsyTuple* newPair = context->createPair(randDist(randEng), randDist(randEng));
+                    IAsyItem* newItem = context->createBlankItem();
+                    AsyFfiHelpers::Item::setItemPtr(newItem, newPair);
+                    newArray->setItem(runIndex, newItem);
+                }
+            },
+            rd(), i
+        ));
+    }
+
+    for (auto& runThread : threads)
+    {
+        runThread.join();
+    }
+
+    returnValue->setRawPointer(newArray);
+}
+
+} // namespace
+
+REGISTER_FN_SIG(threadSample)
+{
+    std::vector const randomPointArgs {
+        AsyFfiHelpers::Types::createFnArgMetadata(AsyFfiHelpers::Types::INT_TYPE, "numberOfPoints")
+    };
+
+    registerer->registerFunction(
+        ASYFFI_FN_NAME_AND_ADDR(createRandomPoints),
+        AsyFfiHelpers::Functions::createFunctionTypeMetadata(
+            AsyFfiHelpers::Types::createArrayType(&AsyFfiHelpers::Types::PAIR_TYPE),
+            STD_CONTAINER_SIZE_AND_DATA(randomPointArgs)
+        )
+    );
+}
