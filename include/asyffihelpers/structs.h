a7f2734fec2ede76c66b36f7572b47bb0f5502c9
Jamie Lindner <jamievlin@outlook.com>
2026-08-12 00:01:10 -0600

    STRUCTS: Add helper functions for structs.

---
+++ include/asyffihelpers/structs.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <asyffi.h>
+#include <stdexcept>
+
+namespace AsyFfiHelpers::Structs
+{
+
+/** Helper class for Asymptote records. This class aids in field retrieval and instance creation. */
+class ManagedRecord
+{
+public:
+    ManagedRecord(IAsyRecord* recordPtr);
+
+    /** Creates a new instance with specified context */
+    IAsyVarFrame* createNewInstance(IAsyContext* context) const;
+
+    /** Returns a pointer to the object under field variableName.
+     * @remark This function is only to be used when there is exactly one field with that name.
+     * When there are multiple fields of the same name but different types, the behavior of this
+     * function is undefined.
+     * In this case, use
+     * {@link ManagedRecord::getField(IAsyVarFrame*, char const*, THAsyType)} instead.
+     */
+    IAsyItem* getField(IAsyVarFrame* recordInstance, char const* variableName) const;
+
+    /** Returns a pointer to the object under field variableName that has a corresponding type */
+    IAsyItem* getField(IAsyVarFrame* recordInstance, char const* variableName, THAsyType type) const;
+
+private:
+    IAsyRecord* record;
+
+    // these variables are obtained from record. If record ever changes,
+    // these variable should also change
+
+    // init lambda from record
+    IAsyLambda* initLambda;
+    IAsyProtoEnvironment* protoEnv;
+    IAsyProtoEnvironment* postDefProtoEnv;
+};
+} // namespace AsyFfiHelpers::Structs
---
+++ src/structs.cc
@@ -0,0 +1,51 @@
+#include "asyffihelpers/structs.h"
+
+namespace AsyFfiHelpers::Structs
+{
+
+ManagedRecord::ManagedRecord(IAsyRecord* recordPtr)
+    : record(recordPtr), initLambda(record->getInitLambda()),
+      protoEnv(record->getProtoEnvironment()),
+      postDefProtoEnv(record->getPostDefinitionProtoEnvironment())
+{
+}
+IAsyVarFrame* ManagedRecord::createNewInstance(IAsyContext* context) const
+{
+    if (initLambda->getClosureRequirement() == Asy::ClosureRequirement::Required)
+    {
+        throw std::runtime_error("TODO: Handle closures");
+    }
+
+    return context->createNewVarFrame(initLambda->getFrameSize());
+}
+IAsyItem* ManagedRecord::getField(IAsyVarFrame* recordInstance, char const* variableName) const
+{
+    THAsyType typePtr = protoEnv->getTypeFromVariable(variableName);
+    if (typePtr == nullptr)
+    {
+        return nullptr;
+    }
+
+    return getField(recordInstance, variableName, typePtr);
+}
+IAsyItem* ManagedRecord::getField(
+    IAsyVarFrame* recordInstance, char const* variableName, THAsyType type
+) const
+{
+    IAsyVarEntry* varEntry = protoEnv->getVarRecord(variableName, type);
+    if (varEntry == nullptr)
+    {
+        return nullptr;
+    }
+
+    IAsyAccess* access = varEntry->getAccess();
+    if (auto const* localAccess = access->tryCastTo(Asy::AccessTypes::Local);
+        localAccess != nullptr)
+    {
+        return recordInstance->getItem(
+            static_cast<IAsyLocalAccess const*>(localAccess)->getOffset()
+        );
+    }
+    return nullptr;
+}
+} // namespace AsyFfiHelpers::Structs
