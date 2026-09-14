57c6d2c671491176f972ac3caee58b98679ac2b9
Jamie Lindner <jamievlin@outlook.com>
2026-09-13 22:19:48 -0600

    HEADER: Add more header files to main asyffihelper.h.

--- include/asyffihelper.h
+++ include/asyffihelper.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <asyffihelpers/args.h>
+#include <asyffihelpers/array.h>
 #include <asyffihelpers/contextFuncs.h>
 #include <asyffihelpers/macros.h>
 #include <asyffihelpers/types.h>
@@ -8,3 +10,5 @@
 #include <asyffihelpers/functions.h>
 #include <asyffihelpers/pen.h>
 #include <asyffihelpers/typesObjs.h>
+#include <asyffihelpers/structs.h>
+#include <asyffihelpers/itemFnStructs.h>
