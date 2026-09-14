f86543fdce365af4cd62787453f3aa8dfd747f8c
Jamie Lindner <jamievlin@outlook.com>
2026-09-13 22:40:24 -0600

    MACROS: Update docs for macros.

--- include/asyffihelpers/macros.h
+++ include/asyffihelpers/macros.h
@@ -3,7 +3,10 @@
 #include <asyffihelpers/contextFuncs.h>
 #include <asyffihelpers/args.h>
 
-/** Add this macro to declare AsyContextHelper with name ctxHelper in a plugin function*/
+/**
+ * Add this macro to declare AsyContextHelper with name ctxHelper in a plugin function.
+ * @see {@link AsyFfiHelpers::Context::AsyContextHelper}
+ */
 #define ASYFFI_CONTEXT_HELPER AsyFfiHelpers::Context::AsyContextHelper ctxHelper(context);
 
 /**
@@ -14,6 +17,8 @@
 /** Shorthand for 0, nullptr */
 #define EMPTY_STD_CONTAINER_SIZE_AND_DATA 0, nullptr
 
-
-/** */
+/**
+ * Shorthand for {@code AsyFfiHelpers::Args::argsToTuple<...>(args) }.
+ * @see {@link AsyFfiHelpers::Args::argsToTuple}
+ */
 #define UNPACK_ARGS(...) AsyFfiHelpers::Args::argsToTuple<__VA_ARGS__>(args);
